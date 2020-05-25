dad505b481bdf4e3e2646204b0d4b183868c66b2
lyzio etancelin <edouard.etancelin@gmail.com>
2020-05-25 20:20:52 +0200

    Typo

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const asyncForEach = async (array, callback) =>  {
 
 const formatAssignee = assignee => `${assignee.login}`
 
-const format_issue = issue => `${issue.title} [github](${issue.html_url})
-    Assign to: ${issue.assignees.map(formatAssignee)}
+const format_issue = issue => `${issue.title}
+    Assign to: ${issue.assignees.map(formatAssignee)}, [github](${issue.html_url})
     
 `
 
